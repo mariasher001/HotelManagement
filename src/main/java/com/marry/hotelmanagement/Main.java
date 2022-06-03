9207b5318f0e8c44d9b6529fa97edf904dcc7d14
mariasher001 <mariasher001@gmail.com>
2022-06-03 23:36:24 +0200

    Version changed from 1.0 to 1.1

--- src/main/java/com/marry/hotelmanagement/Main.java
+++ src/main/java/com/marry/hotelmanagement/Main.java
@@ -2,6 +2,6 @@ package com.marry.hotelmanagement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HotelManagement:)");
+        System.out.println("HotelManagement");
     }
 }
