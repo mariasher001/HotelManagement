147b1a926f6ac4fc909a2dca59bf2151a181496c
mariasher001 <mariasher001@gmail.com>
2022-06-04 19:07:01 +0200

    Started the H2-Database.

--- src/main/java/com/marry/hotelmanagement/Main.java
+++ src/main/java/com/marry/hotelmanagement/Main.java
@@ -1,7 +1,33 @@
 package com.marry.hotelmanagement;
 
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("HotelManagement");
+
+    //Initializing the H2-Server to start the database.
+    private static final Server server = new Server();
+
+    public static void main(String[] args) throws SQLException {
+        startDatabase();
+        System.out.println("Hotel Management");
+        stopDatabase();
+
+    }
+
+    /**
+     * Description: Stops the H2-Database.
+     */
+    private static void stopDatabase() {
+        server.shutdown();
+    }
+
+    /**
+     * Description: Starting the H2-Database.
+     * @throws SQLException: Database Exception
+     */
+    public static void startDatabase () throws SQLException {
+        server.runTool("-tcp","-web","-ifNotExists");
     }
 }
