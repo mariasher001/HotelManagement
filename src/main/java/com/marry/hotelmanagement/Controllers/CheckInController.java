2fc3d0053b18f92c9b5643991b0c7a203e4dbbb8
mariasher001 <mariasher001@gmail.com>
2022-07-29 05:04:32 +0200

    calculateCheckOutDateCheckBox and calculateEstimatedStayCheckBox Added and implemented.

--- src/main/java/com/marry/hotelmanagement/Controllers/CheckInController.java
+++ src/main/java/com/marry/hotelmanagement/Controllers/CheckInController.java
@@ -12,12 +12,16 @@ import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CheckInController {
-
+    @FXML
+    private CheckBox estimatedStayCheckBox;
+    @FXML
+    private CheckBox checkOutDateCheckBox;
     @FXML
     private TextField customerNameTextField;
     @FXML
@@ -35,7 +39,7 @@ public class CheckInController {
     @FXML
     private DatePicker checkInDatePicker;
     @FXML
-    private DatePicker checkOutPicker;
+    private DatePicker checkOutDatePicker;
     @FXML
     private TextField estimatedStayTextField;
     @FXML
@@ -75,7 +79,7 @@ public class CheckInController {
         roomNoList.addAll(roomNumbersOnly);
         roomNoChoiceBox.setItems(roomNoList);
 
-        if(!RoomSearchController.SELECTED_ROOM_NUMBER.equals("")) {
+        if (!RoomSearchController.SELECTED_ROOM_NUMBER.equals("")) {
             roomNoChoiceBox.setValue(RoomSearchController.SELECTED_ROOM_NUMBER);
         }
 
@@ -125,5 +129,46 @@ public class CheckInController {
 
         AllAlerts.confirmAlert("Check-In", "Checked-In!", "Check in was successful");
         Main.setScene("HotelManagementMenu.fxml");
+    }
+
+    public void checkOutDateCheckBoxClicked(ActionEvent actionEvent) {
+        if (checkOutDateCheckBox.isSelected()) {
+            if (checkInDatePicker.getValue() != null && !estimatedStayTextField.getText().isEmpty()) {
+                if (Integer.parseInt(estimatedStayTextField.getText()) < 0) {
+                    AllAlerts.errorAlert("Negative Numbers", "Negative Numbers in estimated stay", "Please Use Positive integers only!");
+                    checkOutDateCheckBox.setSelected(false);
+                    return;
+                }
+                int NumberOfDays = Integer.parseInt(estimatedStayTextField.getText());
+                checkOutDatePicker.setValue(checkInDatePicker.getValue().plusDays(NumberOfDays));
+                checkOutDatePicker.setEditable(false);
+            } else {
+                AllAlerts.errorAlert("EmptyFields", "Empty Fields Detected", "CheckIn and CheckOutDates fields cannot be left empty!!");
+                checkOutDateCheckBox.setSelected(false);
+            }
+
+        } else {
+            checkOutDatePicker.setEditable(true);
+        }
+    }
+
+    public void estimatedStayCheckBoxClicked(ActionEvent actionEvent) {
+        if (estimatedStayCheckBox.isSelected()) {
+            if (checkInDatePicker.getValue() != null && checkOutDatePicker.getValue() != null) {
+                if (checkOutDatePicker.getValue().isAfter(checkInDatePicker.getValue())) {
+                    long estimatedTime = Duration.between(checkInDatePicker.getValue().atStartOfDay(), checkOutDatePicker.getValue().atStartOfDay()).toDays();
+                    estimatedStayTextField.setText("" + estimatedTime);
+                    estimatedStayTextField.setEditable(false);
+                } else {
+                    AllAlerts.errorAlert("WrongCheckOutDate", "Wrong check out date", "Check-out date should not be after check-in date. Please re-enter!!");
+                    estimatedStayCheckBox.setSelected(false);
+                }
+            } else {
+                AllAlerts.errorAlert("EmptyFields", "Empty Fields Detected", "CheckIn and CheckOutDates fields cannot be left empty!!");
+                estimatedStayCheckBox.setSelected(false);
+            }
+        } else {
+            estimatedStayTextField.setEditable(true);
+        }
     }
 }
